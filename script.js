c51bb48a2646be8b78e8af36afc0a56112d8c732
Sameer Kamble <sameerkamble1130@gmail.com>
2025-01-02 14:14:21 +0530

    Initialiation, NavBar and Hero Section

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const navDialog = document.getElementById("nav-dialog");
+function handleMenu(){
+    // console.log("clicked");
+    navDialog.classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+    //   fontFamily: {
+    // },
+    colors: {
+      'primary': '#3238f2',
+    }
+  },
+  plugins: [],
+}
+}
